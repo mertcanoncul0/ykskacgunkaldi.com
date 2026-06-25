8e14c78f35e4d550c0d44b25e86db2cbec55c0dd
Mertcan Öncül <mertcanoncul@icloud.com>
2026-06-25 10:51:02 +0300

    fix: indexnow key route çakışmasını kaldır

--- src/pages/df7cdb4351ae6da7.txt.ts
+++
@@ -1,8 +0,0 @@
-export function GET() {
-  return new Response("df7cdb4351ae6da7", {
-    headers: {
-      "Content-Type": "text/plain; charset=utf-8",
-      "Cache-Control": "public, max-age=86400",
-    },
-  });
-}
